098efce71a68f755bae47e709d5aff304808dac4
DanisSit <144054631+DanisSit@users.noreply.github.com>
2023-10-11 14:50:56 +0500

    продолжение работы

--- pr8/action.php
+++ pr8/action.php
@@ -10,6 +10,8 @@ $dbname     = "u19978_a09";    // Имя базы данных
 $user       = "u19978_a09";           // Имя пользователя
 $password   = "I1w6G5a9I0";               // Пароль
 
+if(empty($_POST['login'])) exit('Не указан Логин');
+if(empty($_POST['password'])) exit('Не указан пароль');
 
 try {
     $connection = new PDO('mysql:host='.$host.';dbname='.$dbname.';charset=utf8', $user, $password);
@@ -17,6 +19,17 @@ try {
     echo "Ошибка подключения"; return;
 }
 
+$select = $connection->prepare( "SELECT * FROM `users` WHERE `login` = ? OR `email` = ?;" ); 
+    $res = $select->execute([ $_POST['login'],$_POST['email'] ] );
+    $row = $select->fetch();
+
+    if(!$res ){
+        exit( 'Ошибка регистрации...');
+    }
+
+    if( $res && isset($row['cnt']) && $row[0] > 0 ){
+    exit( 'Ошибка регистрации... (Пользователь уже существует)' );
+    }
 
 ?>
 </body>
