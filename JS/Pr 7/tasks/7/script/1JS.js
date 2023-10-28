0993efbbdd07b12a6ed11d5789077620505c44fd
DanisSit <144054631+DanisSit@users.noreply.github.com>
2023-10-28 14:35:15 +0500

    задание 7 пр 7 завершение

--- JS/Pr 7/tasks/7/script/1JS.js
+++ JS/Pr 7/tasks/7/script/1JS.js
@@ -1,15 +1,12 @@
 let a = ["ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ", "ВС"];
-let Year = Data.getFullYear();
-let Month = Data.getMonth();
-let Day = Data.getDate();
+let Day = "СБ";
 
 for (let i = 0; i < 7; i++) {
-  if (a[i] = getWeekDay(Year, Month, Day)){
+  if (a[i] == Day){
     const style = 'font-style: italic; font-size: 2em;'
     console.log("%c"+a[i]+"", style);
   } else{
     console.log(a[i]);
   }
 }
-console.log(Day);
 
